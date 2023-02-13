f46d2a3e9d4dbda9c66fee96f563d8e6b7726eb2
yogi1433 <121537038+yogi1433@users.noreply.github.com>
2023-02-13 17:56:13 +0530

    hds

---
+++ Po2.java
@@ -0,0 +1,24 @@
+class Po2
+{
+public static void main(String[] args)
+{
+int star=1;
+int space=5;
+{
+for(int i=1;i<=5;i++)
+{
+for(int j=1;j<=star;j++)
+{
+System.out.print(" ");
+}{
+for(int k=1;k<=space;k++)
+{
+System.out.print("* ");
+}
+{System.out.println();
+space--;
+star++;
+}
+}}
+}
+}}
